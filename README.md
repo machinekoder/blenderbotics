f1f70bb59b5d3561636ba25acd7aa7b9cb113c73
Alexander Rössler <mail@roessler.systems>
2020-04-06 11:55:44 +0200

    add start script

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
+## How to Use
+
+Open the Blender file and load the `scripts/script.py`.
+
+You now should see a new tool panel inside Blender for controlling the robot.
+
 ## Coding style
 
 ### Pre-Commit
-The Docker container comes with [pre-commit](https://pre-commit.com/) installed.
+The repository comes with [pre-commit](https://pre-commit.com/) installed.
 pre-commit is a pre-commit framework which can be attached as git hook. It
 runs code-formatting and other checks automatically before committing any code.
 You just need to install with:
---
+++ scripts/script.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import sys
+import importlib
+from types import ModuleType
+
+import robot_control
+
+
+def deep_reload(m: ModuleType):
+    name = m.__name__  # get the name that is used in sys.modules
+    name_ext = name + '.'  # support finding sub modules or packages
+
+    def compare(loaded: str):
+        return (loaded == name) or loaded.startswith(name_ext)
+
+    all_mods = tuple(sys.modules)  # prevent changing iterable while iterating over it
+    sub_mods = filter(compare, all_mods)
+    for pkg in sorted(sub_mods, key=lambda item: item.count('.'), reverse=True):
+        importlib.reload(
+            sys.modules[pkg]
+        )  # reload packages, beginning with the most deeply nested
+
+
+if __name__ == '__main__':
+    robot_control.unregister()
+    deep_reload(robot_control)
+    robot_control.register()
